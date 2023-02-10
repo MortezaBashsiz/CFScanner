fffd340563811085e7888874dd5c8699b7159dff
Mohammad Mahdi Bakhshi <bakhshi@protonmail.com>
2023-02-10 21:49:36 +0330

    modify naming format from snake_case to camelCase

--- scripts/cfFindIP.sh
+++ scripts/cfFindIP.sh
@@ -54,9 +54,9 @@ configPort="NULL"
 configPath="NULL"
 configServerName="NULL"
 
-bar_char_done="#"
-bar_char_todo="-"
-bar_percentage_scale=2
+barCharDone="#"
+barCharTodo="-"
+barPercentageScale=2
 progressBar=""
 
 # Check if config file exists
@@ -88,23 +88,23 @@ fi
 
 # Progress bar maker function
 # Base on https://www.baeldung.com/linux/command-line-progress-bar
-function show_progress {
+function showProgress {
   current="$1"
   total="$2"
 
-  bar_size="$(($(tput cols)-60))" # 65 cols for description characters
+  barSize="$(($(tput cols)-60))" # 65 cols for description characters
 
   # calculate the progress in percentage 
-  percent=$(bc <<< "scale=$bar_percentage_scale; 100 * $current / $total" )
+  percent=$(bc <<< "scale=$barPercentageScale; 100 * $current / $total" )
   # The number of done and todo characters
-  done=$(bc <<< "scale=0; $bar_size * $percent / 100" )
-  todo=$(bc <<< "scale=0; $bar_size - $done")
+  done=$(bc <<< "scale=0; $barSize * $percent / 100" )
+  todo=$(bc <<< "scale=0; $barSize - $done")
   # build the done and todo sub-bars
-  done_sub_bar=$(printf "%${done}s" | tr " " "${bar_char_done}")
-  todo_sub_bar=$(printf "%${todo}s" | tr " " "${bar_char_todo}")
+  doneSubBar=$(printf "%${done}s" | tr " " "${barCharDone}")
+  todoSubBar=$(printf "%${todo}s" | tr " " "${barCharTodo}")
 
   # output the bar
-  progressBar="Progress : [${done_sub_bar}${todo_sub_bar}] ${percent}% of total IPs.         " # Some end space for pretty formatting
+  progressBar="Progress : [${doneSubBar}${todoSubBar}] ${percent}% of total IPs.         " # Some end space for pretty formatting
 }
 
 # Function fncCheckSubnet
@@ -211,7 +211,7 @@ do
   passedIpsCount=0
 	for subNet in ${cloudFlareIpList}
 	do
-    show_progress $passedIpsCount $ipListLength
+    showProgress $passedIpsCount $ipListLength
 		firstOctet=$(echo "$subNet" | awk -F "." '{ print $1 }')
 		if [[ "${cloudFlareOkList[*]}" =~ $firstOctet ]]
 		then
