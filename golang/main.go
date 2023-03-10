231d496df14427251c463cbc219babd1ba410015
Sonya <sonyacore@protonmail.com>
2023-03-10 15:11:58 +0330

    updated main & cobra commands

--- golang/main.go
+++ golang/main.go
@@ -6,6 +6,7 @@ import (
 	utils "CFScanner/utils"
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"runtime"
 	"strings"
@@ -22,6 +23,7 @@ var config = scan.ConfigStruct{
 	Ws_header_path:       "",
 	Sni:                  "",
 	Do_upload_test:       false,
+	Do_fronting_test:     false,
 	Min_dl_speed:         50.0,
 	Min_ul_speed:         50.0,
 	Max_dl_time:          -2.0,
@@ -36,7 +38,7 @@ var config = scan.ConfigStruct{
 
 // Program Info
 var (
-	version  = "0.5"
+	version  = "0.6"
 	build    = "Custom"
 	codename = "CFScanner , CloudFlare Scanner."
 )
@@ -56,7 +58,7 @@ func main() {
 	var threads int
 	var configPath string
 	var noVpn bool
-	var subnetsPath string
+	var subnets string
 	var doUploadTest bool
 	var nTries int
 	var minDLSpeed float64
@@ -68,6 +70,7 @@ func main() {
 	var frontingTimeout float64
 	var maxDLLatency float64
 	var maxULLatency float64
+	var fronting bool
 
 	var bigIPList []string
 
@@ -78,39 +81,58 @@ func main() {
 			fmt.Println(VersionStatement())
 			if !noVpn {
 				utils.CreateDir(scan.CONFIGDIR)
-				// configfilepath := configPath
 			}
 			utils.CreateDir(scan.RESULTDIR)
 			if err := configuration.CreateInterimResultsFile(scan.INTERIM_RESULTS_PATH, nTries); err != nil {
 				fmt.Printf("Error creating interim results file: %v\n", err)
 			}
+			// number of threads for scanning
 			threadsCount := threads
 
+			// lists of ip for scanning proccess
 			var IPLIST []string
-			if subnetsPath != "" {
-				subnetFilePath := subnetsPath
-				subnetFile, err := os.Open(subnetFilePath)
-				if err != nil {
-					panic(err)
-				}
-				defer subnetFile.Close()
 
-				scanner := bufio.NewScanner(subnetFile)
-				for scanner.Scan() {
-					IPLIST = append(IPLIST, strings.TrimSpace(scanner.Text()))
+			file, _ := utils.Exists(subnets)
+			if file {
+				if subnets != "" {
+					subnetFilePath := subnets
+					subnetFile, err := os.Open(subnetFilePath)
+					if err != nil {
+						log.Fatal(err)
+					}
+					defer subnetFile.Close()
+
+					scanner := bufio.NewScanner(subnetFile)
+					for scanner.Scan() {
+						IPLIST = append(IPLIST, strings.TrimSpace(scanner.Text()))
+					}
+					if err := scanner.Err(); err != nil {
+						log.Fatal(err)
+					}
 				}
-				if err := scanner.Err(); err != nil {
-					panic(err)
+			} else {
+				// Parsing cidr input
+				if strings.Contains(subnets, "/") {
+					var subnetips []string
+					subnetips = append(subnetips, subnets)
+
+					ips := utils.IPParser(subnetips)
+
+					IPLIST = append(IPLIST, ips...)
+				} else {
+					// Parsing ip input
+					var validateip string = utils.IPValidator(subnets)
+					IPLIST = append(IPLIST, validateip)
 				}
-				// } else {
-				// 	cidrList = readCidrsFromAsnLookup()
-				// }
 			}
+
+			// Create Configuration file
 			testConfig := configuration.CreateTestConfig(configPath, time.Duration(startProcessTimeout), doUploadTest,
 				minDLSpeed, minULSpeed, maxDLTime, maxULTime,
-				frontingTimeout, maxDLLatency, maxULLatency,
+				frontingTimeout, fronting, maxDLLatency, maxULLatency,
 				nTries, noVpn)
 
+			// Total number of IPS
 			var nTotalIPs int
 
 			for _, ips := range IPLIST {
@@ -118,6 +140,7 @@ func main() {
 				nTotalIPs += numIPs
 			}
 
+			// Parsing and Validanting IPLISTS
 			bigIPList = utils.IPParser(IPLIST)
 
 			fmt.Println("Total Threads : ", utils.Colors.OKBLUE, threads, utils.Colors.ENDC)
@@ -127,11 +150,12 @@ func main() {
 			fmt.Println("Results Written in :", scan.INTERIM_RESULTS_PATH)
 		},
 	}
-	rootCmd.PersistentFlags().IntVar(&threads, "threads", 1, "Number of threads to use for parallel computing")
-	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "The path to the config file. For confg file example, see https://github.com/MortezaBashsiz/CFScanner/blob/main/bash/ClientConfig.json")
+	rootCmd.PersistentFlags().IntVarP(&threads, "threads", "t", 1, "Number of threads to use for parallel scanning")
+	rootCmd.PersistentFlags().StringVarP(&configPath, "config", "c", "", "The path to the config file. For config file example, see https://github.com/MortezaBashsiz/CFScanner/blob/main/bash/ClientConfig.json")
 	rootCmd.PersistentFlags().BoolVar(&noVpn, "novpn", false, "If passed, test without creating vpn connections")
-	rootCmd.PersistentFlags().StringVar(&subnetsPath, "subnets", "", "(optional) The path to the custom subnets file. each line should be in the form of ip.ip.ip.ip/subnet_mask or ip.ip.ip.ip. If not provided, the program will read the cidrs from asn lookup")
+	rootCmd.PersistentFlags().StringVarP(&subnets, "subnets", "s", "", "The file or subnet. each line should be in the form of ip.ip.ip.ip/subnet_mask or ip.ip.ip.ip.")
 	rootCmd.PersistentFlags().BoolVar(&doUploadTest, "upload-test", false, "If True, upload test will be conducted")
+	rootCmd.PersistentFlags().BoolVar(&fronting, "fronting-test", false, "If True, fronting request test will be conducted")
 	rootCmd.PersistentFlags().IntVar(&nTries, "tries", 1, "Number of times to try each IP. An IP is marked as OK if all tries are successful")
 	rootCmd.PersistentFlags().Float64Var(&minDLSpeed, "download-speed", 50, "Minimum acceptable download speed in kilobytes per second")
 	rootCmd.PersistentFlags().Float64Var(&minULSpeed, "upload-speed", 50, "Maximum acceptable upload speed in kilobytes per second")
