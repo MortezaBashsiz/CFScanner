6527a560f4c54e1ea141f582e78ab71bcd6de49b
Sonya <sonyacore@protonmail.com>
2023-03-29 03:28:05 +0330

    updated configuration structs

--- golang/configuration/types.go
+++ golang/configuration/types.go
@@ -1,22 +1,40 @@
 package config
 
 type ConfigStruct struct {
-	Local_port           int
-	Address_port         string
-	User_id              string
-	Ws_header_host       string
-	Ws_header_path       string
-	Sni                  string
-	Do_upload_test       bool
-	Do_fronting_test     bool
-	Min_dl_speed         float64 // kilobytes per second
-	Min_ul_speed         float64 // kilobytes per second
-	Max_dl_time          float64 // seconds
-	Max_ul_time          float64 // seconds
-	Max_dl_latency       float64 // seconds
-	Max_ul_latency       float64 // seconds
-	Fronting_timeout     float64 // seconds
+	Local_port       int
+	Address_port     string
+	User_id          string
+	Ws_header_host   string
+	Ws_header_path   string
+	Sni              string
+	Fronting_timeout float64 // seconds
+	N_tries          int
+	TestBool
+}
+
+type TestBool struct {
+	Do_upload_test   bool
+	Do_fronting_test bool
+}
+
+type Download struct {
+	Min_dl_speed   float64 // kilobytes per second
+	Max_dl_time    float64 // seconds
+	Max_dl_latency float64 // seconds
+
+}
+
+type Upload struct {
+	Min_ul_speed   float64 // kilobytes per second
+	Max_ul_time    float64 // seconds
+	Max_ul_latency float64 // seconds
+
+}
+
+type Worker struct {
+	Download             Download
+	Upload               Upload
 	Startprocess_timeout float64 // seconds
-	N_tries              int
+	Threads              int
 	Vpn                  bool
 }
